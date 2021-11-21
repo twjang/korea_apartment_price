2293828bb12f26569e5c20a1750ddce4ce0ccf7e
nyamnyam <nyamnyam7@gmail.com>
2021-11-21 13:21:08 +0900

    feat: updated report format

--- scripts/gen_report.py
+++ scripts/gen_report.py
@@ -55,11 +55,13 @@ def render_graph(apt: ApartmentId, date_from=20210101)->Tuple[str, FigureWidget]
   trades = korea_apartment_price.db.query_trades(apt_ids=[apt], size_from=chosen_size-0.9, size_to=chosen_size+0.9, date_from=date_from, include_canceled=True)
   trades_x = [date_serial2date(t['date_serial']) for t in trades if not t['is_canceled']]
   trades_y = [t['price'] / 10000 for t in trades if not t['is_canceled']]
+  labels = [f'{t["floor"]}층' for t in trades if not t['is_canceled']]
+
   canceled_trades_x = [date_serial2date(t['date_serial']) for t in trades if t['is_canceled']]
   canceled_trades_y = [t['price'] / 10000 for t in trades if t['is_canceled']]
-
-  el = go.Scatter(x=trades_x, y=trades_y, showlegend = False, marker={'color': 'blue', 'size': 10}, mode='markers', name='실거래')
-  el_canceled = go.Scatter(x=canceled_trades_x, y=canceled_trades_y, showlegend = False, marker={'color': 'orange', 'size': 10, 'symbol': 'x'}, mode='markers', name='취소거래')
+  canceled_labels = [f'{t["floor"]}층(취소)' for t in trades if t['is_canceled']]
+  el = go.Scatter(x=trades_x, y=trades_y, showlegend = False, marker={'color': 'blue', 'size': 10}, mode='markers', hovertext=labels, name='실거래')
+  el_canceled = go.Scatter(x=canceled_trades_x, y=canceled_trades_y, showlegend = False, marker={'color': 'orange', 'size': 10, 'symbol': 'x'}, mode='markers', hovertext=canceled_labels, name='취소')
   fig.add_trace(el)
   fig.add_trace(el_canceled)
 
@@ -67,11 +69,28 @@ def render_graph(apt: ApartmentId, date_from=20210101)->Tuple[str, FigureWidget]
   kb_orderbook = sorted(korea_apartment_price.db.query_kb_orderbook(apt, size_from=chosen_size-1, size_to=chosen_size+1, fetched_from=date_from), key=lambda x: x['fetched_at'])
   fetched_date_cnt = {}
   fetched_price_date_cnt = {}
+  fetched_price_date_lbls = {}
   for od in kb_orderbook:
     date_end = od['fetched_at']
-    price = od['price'] / 10000
+    if od['detail']['최소매매가'] is not None:
+      price = int(od['detail']['최소매매가']) / 10000
+    else:
+      price = od['price'] / 10000
     fetched_date_cnt[date_end] = fetched_date_cnt.get(date_end, 0) + 1
     fetched_price_date_cnt[(date_end, price)] = fetched_price_date_cnt.get((date_end, price), 0) + 1
+    if not (date_end, price) in fetched_price_date_lbls:
+      fetched_price_date_lbls[(date_end, price)] = []
+
+    curlbl = ''
+    if od['apt_dong'] is not None and len(od['apt_dong']) > 0:
+      curlbl += f'{od["apt_dong"]}동'
+    if od['apt_ho'] is not None and len(od['apt_ho']) > 0:
+      curlbl += f'{od["apt_ho"]}호'
+    elif od['floor'] is not None and len(od['floor']) > 0:
+      curlbl += f'{od["floor"]}'
+    if curlbl == '': curlbl='정보없음'
+    fetched_price_date_lbls[(date_end, price)].append(curlbl)
+
   fetched_dates = sorted(fetched_date_cnt.keys())
 
   max_cnt = max([1] + list(fetched_price_date_cnt.values()))
@@ -88,7 +107,9 @@ def render_graph(apt: ApartmentId, date_from=20210101)->Tuple[str, FigureWidget]
 
     opacity = min(1.0, 0.1 + 0.9 * cnt / max_cnt)
     fig.add_shape(x0=date_start, x1=date_end, y0=price, y1=price, line={'width':3, 'color':'red'}, opacity=opacity)
-    marker = go.Scatter(x=[date_end], y=[price], text=[f'{price}억: {cnt}개'], marker={'color':'red', 'size':4}, opacity=opacity, showlegend = False, name='')
+    details = fetched_price_date_lbls[(date_end, price)]
+    details = '<br>' + '<br>'.join(sorted(details))
+    marker = go.Scatter(x=[date_end], y=[price], text=[f'{cnt}개 {details}'], marker={'color':'red', 'size':4}, opacity=opacity, showlegend = False, name='')
     fig.add_trace(marker)
 
   return title, fig
